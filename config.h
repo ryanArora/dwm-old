31d0f508dfd42d0a6cb03600f0ef5dc39cbbdac1
Ryan Arora <arora.ryan@gmail.com>
2022-04-03 19:48:31 -0400

    fix: consistent config.h name, change font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "DejaVuSans Mono:size=10:antialias=true:autohint=true", "Font Awesome 6 Free:size=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
