5af504787e9db677078261f6fbaf5f117fa7ca55
Ryan Arora <arora.ryan@gmail.com>
2022-04-08 11:14:08 -0400

    feat: swallow st

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVuSans Mono:size=10:antialias=true:autohint=true", "Font Awesome 6 Free:size=16:antialias=true:autohint=true" };
@@ -27,9 +28,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class            instance    title   tags    mask    isfloating  isterminal  noswallow  monitor */
+	{ "st-256color",    NULL,       NULL,   0,      0,      1,          0,          -1 },
 };
 
 /* layout(s) */
